2cbcec01b88fb5874390e833b74b0dac17493a6e
u-servicespb <128871915+u-servicespb@users.noreply.github.com>
2023-08-01 11:07:26 +0800

    Add files via upload

---
+++ src/main/java/koschei/models/Duck5.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+public class Duck5 {
+
+    private Egg6 egg;
+
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
+
+    @Override
+    public String toString() {
+        return ", в утке яйцо " + egg.toString();
+    }
+}
